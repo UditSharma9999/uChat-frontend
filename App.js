9ebc39e66701bac9702a9fbff4fce703e9289b3d
udit sharma <63443176+UditSharma9999@users.noreply.github.com>
2022-03-04 12:27:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { LogBox } from 'react-native';
-// import Profile from './src/Profile';
 import ChatRoom from './src/ChatRoom';
 import SearchScreen from './src/SearchScreen';
 import SignIn from './src/SignIn';
@@ -14,31 +13,12 @@ LogBox.ignoreAllLogs();  //For Reamoving Warnnings
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
-// const horizontalAnimation = {
-//   gestureDirection: 'horizontal',
-//   cardStyleInterpolator: ({ current, layouts }) => {
-//     return {
-//       cardStyle: {
-//         transform: [
-//           {
-//             translateX: current.progress.interpolate({
-//               inputRange: [0, 1],
-//               outputRange: [layouts.screen.width, 0],
-//             }),
-//           },
-//         ],
-//       },
-//     };
-//   },
-// };
-
 const Stack = createStackNavigator();
 const App =() => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        // screenOptions={horizontalAnimation}
+        
         initialRouteName='SignUp'
       >
         <Stack.Screen
